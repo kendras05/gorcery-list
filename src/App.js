79a73e4301c635dbcf7689a22e8535ff24358ad4
kendras05 <kendrapsy@gmail.com>
2020-02-17 14:15:01 -0800

    fixed error

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ return (
 		<div className="list">
 		<textarea onChange={storeListItems}></textarea>
 		<button onClick={handleButtonClick}>Save</button>
-		<ul><input type="checkbox">{listItems}</input></ul>
+		<ul>{listItems}</ul>
 		</div>
  );
 }
